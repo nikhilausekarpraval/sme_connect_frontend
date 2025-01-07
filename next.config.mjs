c704eb1246945f91668782b9d492e3d5278f8a17
Nikhil Ausekar <nikhil.a@pravaltech.com>
2025-01-07 11:38:29 +0530

    stopped eslint

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+export default {
+    eslint: {
+      ignoreDuringBuilds: true, // Ignores ESLint during build
+    },
+  };
 
-export default nextConfig;
