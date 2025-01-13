84dcdd0446d3f6109329239956b55ed5335848fc
Nikhil Ausekar <nikhil.a@pravaltech.com>
2025-01-13 15:49:37 +0530

    practice updated with service

--- src/app/Components/PracticeCard/PracticeCard.tsx
+++ src/app/Components/PracticeCard/PracticeCard.tsx
@@ -3,49 +3,39 @@ import { routes } from '@/app/Constants/Constants';
 import { useRouter } from 'next/navigation';
 import React from 'react';
 import { Card } from 'react-bootstrap';
-import { FaArrowRight } from 'react-icons/fa'; 
+import { FaArrowRight } from 'react-icons/fa';
 import './PracticeCard.scss';
 
 type DepartmentCardProps = {
-    title: string;
-    description: string[];
+    name: string;
+    description: string;
 };
 
-const PracticeCard: React.FC<DepartmentCardProps> = ({ title, description }) => {
+const PracticeCard: React.FC<DepartmentCardProps> = ({ name, description }) => {
     const router = useRouter();
 
     const handleNavigation = () => {
-        const data = JSON.stringify({ key: { title } });
+        const data = JSON.stringify({ key: { title: name } });
         router.push(`${routes.practiceDashboard}?data=${encodeURIComponent(data)}`);
     };
 
     return (
-        <Card 
-            className="shadow-md cursor-pointer rounded-lg card-hover-effect mb-3" 
-            onClick={handleNavigation} 
-            style={{ transition: 'transform 0.3s ease, box-shadow 0.3s ease' }}
+        <Card
+            className="card-container shadow-md cursor-pointer card-hover-effect"
+            onClick={handleNavigation}
         >
-            <Card.Body style={{ padding: '20px' }}>
-                <Card.Title 
-                    className="text-primary pt-2 pb-3 font-weight-bold mb-2" 
-                    style={{ fontSize: '1.25rem', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}
-                >
-                    {title}
-                    <FaArrowRight style={{ fontSize: '1rem', color: '#0056D2' }} />
+            <Card.Body className="card-body">
+                <Card.Title className="card-title text-blue-500">
+                    {name}
+                    <FaArrowRight className="arrow-icon" />
                 </Card.Title>
-
-                <Card.Text style={{ color: '#616161', fontSize: '0.95rem', lineHeight: '1.5' }}>
-                    <strong style={{ fontSize: '0.9rem', color: '#333' }}>Description:</strong>
-                    <ul style={{ paddingLeft: '20px', marginTop: '8px' }}>
-                        {description.map((item, index) => (
-                            <li key={index} style={{ marginBottom: '4px' }}>
-                                {item}
-                            </li>
-                        ))}
-                    </ul>
+                <Card.Text className="card-text">
+                    <strong className="card-description-label">Description:</strong>
+                    <div className="card-description text-black">{description}</div>
                 </Card.Text>
             </Card.Body>
         </Card>
+
     );
 };
 
--- src/app/Dashboard/Practices/page.tsx
+++ src/app/Dashboard/Practices/page.tsx
@@ -1,14 +1,36 @@
+'use client'
 import PracticeCard from '@/app/Components/PracticeCard/PracticeCard'
-import { practicesData, practicesList } from '@/app/Constants/Constants'
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './page.scss';
+import PracticesService from '@/app/Services/PracticesService';
+import { IPractice } from '@/app/Interfaces/Interfaces';
 
 export default function page() {
+
+    const [practices,setPractces] = useState<IPractice[]>();
+    const practiceService = new PracticesService();
+    
+       useEffect(() => {
+
+               getData();
+       }, []);
+
+       const getData =async()=>{
+        try {
+            const practiceData = await practiceService.getPractices();
+            setPractces(practiceData?.value?.data);
+        } catch (error) {
+            
+        }
+
+       }
+
+
     return (
-        <div className="overflow-auto flex flex-1 flex-col">
-            <div className="grid-container overflow-auto ">
-                {practicesList.map((department, index) => (
-                    <PracticeCard key={index} {...department} />
+        <div className="overflow-auto h-100 flex flex-1 flex-col">
+            <div className="practice-grid-container h-100 overflow-auto ">
+                {practices?.map((practice, index) => (
+                    <PracticeCard key={index} {...practice} />
                 ))}
             </div>
         </div>
