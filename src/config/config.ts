bf64938e9d41a1ed8afdac7e794877125e51179e
Nikhil Ausekar <nikhil.a@pravaltech.com>
2025-02-24 18:39:35 +0530

    updated config

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,8 +5,8 @@ export const config = {
  
         authority: 'https://login.microsoftonline.com/{648ca8d9-38e9-44ca-bc27-20e5a79ee859}',
         clientId: '5bfa2aed-bd36-44f2-a920-f0803d1f7b62',
-        redirectUri: "http:localhost:3000",
-        postLogoutRedirectUri:"http:localhost:3000"
+        redirectUri: process.env.NEXT_PUBLIC_AZURE_AD_REDIRECT_URI,
+        postLogoutRedirectUri: process.env.NEXT_PUBLIC_AZURE_AD_REDIRECT_URI
  
     },
     cache: {
