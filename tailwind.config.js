fdc0057b2a0617752620740daa16c4ccfc889bcb
Nikhil Ausekar <nikhil.a@pravaltech.com>
2025-03-06 11:16:28 +0530

    removed tw-elements path

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,15 +12,13 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/tw-elements/js/**/*.js",
      './node_modules/preline/preline.js',
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/tw-elements/js/**/*.js"
   ],
   theme: {
     extend: {},
   },
   darkMode: "class",
-  plugins: ["tw-elements/plugin.cjs", require('preline/plugin')]
+  plugins: [require('preline/plugin')]
 }
